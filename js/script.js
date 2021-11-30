4c3dce72adc8e1f3c8bca9166426a6219d3392b1
Moriturum01 <chi4ckov@yandex.ru>
2021-12-01 00:30:11 +0300

    9

---
+++ js/script.js
@@ -0,0 +1,89 @@
+"use strict";
+
+const calculate = document.getElementById("start");
+
+console.log(calculate);
+
+const btnPlusIncomeAdd = document.getElementsByTagName("button")[0];
+
+console.log(btnPlusIncomeAdd);
+
+const btnPlusExpensesAdd = document.getElementsByTagName("button")[1];
+
+console.log(btnPlusExpensesAdd);
+
+const checkbox = document.querySelector("#deposit-check");
+
+console.log(checkbox);
+
+const additionalIncomeItem = document.querySelectorAll(
+  ".additional_income-item"
+);
+
+console.log(additionalIncomeItem);
+
+const budgetMonth = document.getElementsByClassName("budget_month-value");
+
+console.log(budgetMonth);
+
+const budgetDay = document.getElementsByClassName("budget_day-value");
+
+console.log(budgetDay);
+
+const expensesMonth = document.getElementsByClassName("expenses_month-value");
+
+console.log(expensesMonth);
+
+const additionalIncome = document.getElementsByClassName(
+  "additional_income-value"
+);
+
+console.log(additionalIncome);
+
+const additionalExpenses = document.getElementsByClassName(
+  "additional_expenses-value"
+);
+
+console.log(additionalExpenses);
+
+const incomePeriod = document.getElementsByClassName("income_period-value");
+
+console.log(incomePeriod);
+
+const targetMonth = document.getElementsByClassName("target_month-value");
+
+console.log(targetMonth);
+
+const salaryAmount = document.querySelector(".salary-amount");
+
+console.log(salaryAmount);
+
+const incomeTitle = document.querySelector(".income-title");
+
+console.log(incomeTitle);
+
+const incomeAmount = document.querySelector(".income-amount");
+
+console.log(incomeAmount);
+
+const aditionalIncomeItem = document.querySelector(".additional_income-item");
+
+console.log(aditionalIncomeItem);
+
+const expensesTitle = document.querySelector(".expenses-title");
+
+console.log(expensesTitle);
+
+const expensesAmount = document.querySelector(".expenses-amount");
+
+console.log(expensesAmount);
+
+const additionalExpensesItem = document.querySelector(
+  ".additional_expenses-item"
+);
+
+console.log(additionalExpensesItem);
+
+const range = document.querySelector('input[type="range"]');
+
+console.log(range);
